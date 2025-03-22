c50d0049ec75b03dbb92b94fa4842d928aae1a9e
Alec Zaitz <a02423683@usu.edu>
2025-03-22 12:37:44 -0600

    Fixed some styling

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -9,14 +9,16 @@ import { BackButton } from "../navigation/BackButton";
 interface PageLayoutProps {
   children: React.ReactNode;
   showBackButton?: boolean;
+  fullWidth?: boolean;
 }
 
 export const PageLayout: React.FC<PageLayoutProps> = ({
   children,
   showBackButton = true,
+  fullWidth = false,
 }) => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className={fullWidth ? "" : "container mx-auto px-4 py-8"}>
       {showBackButton && (
         <div className="mb-6">
           <BackButton />
