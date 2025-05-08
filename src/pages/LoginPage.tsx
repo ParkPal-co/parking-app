72b78c6fc1bcfea311f2ae286d0169d67da52b4c
Alec Zaitz <a02423683@usu.edu>
2025-05-08 08:18:14 -0600

    cosmetic fix

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -113,11 +113,7 @@ const LoginPage: React.FC = () => {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-gray-50 text-gray-500">
-                Or continue with
-              </span>
-            </div>
+            <div className="relative flex justify-center text-sm" />
           </div>
 
           <div className="mt-6">
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -114,11 +114,7 @@ const RegisterPage: React.FC = () => {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-gray-50 text-gray-500">
-                Or continue with
-              </span>
-            </div>
+            <div className="relative flex justify-center text-sm" />
           </div>
 
           <div className="mt-6">
