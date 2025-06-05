a9408dab18b8f26b5e82dbf1b0e081eead35a73e
Alec Zaitz <a02423683@usu.edu>
2025-06-05 16:26:24 -0600

    random fix

--- src/components/parking/DrivewayListingsPanel.tsx
+++ src/components/parking/DrivewayListingsPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ParkingSpot } from "../../types";
 import { ParkingSpotCard } from "./ParkingSpotCard";
 import { addEventNotificationEmail } from "../../services/events/eventNotificationService";
